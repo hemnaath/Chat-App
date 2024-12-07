fae55ed19e628199bd69a1840c144a4cc0d1ca73
hemnaath <45599791+hemnaath@users.noreply.github.com>
2024-12-08 03:32:49 +0530

    Create passport.js

---
+++ config/passport.js
@@ -0,0 +1,23 @@
+const passport = require('passport');
+const { Strategy: GoogleStrategy } = require("passport-google-oauth20");
+
+passport.use(
+    new GoogleStrategy({
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    (accessToken, refreshToken, profile, done) => {
+        done(null, profile);
+    })
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+module.exports = passport;
